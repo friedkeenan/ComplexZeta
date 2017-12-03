5757428d90c1a9ed19a7d1cbacdd9be954bf264b
friedkeenan <friedkeenan@gmail.com>
2017-12-03 12:15:49 -0600

    Added more controls
    Scroll to change scale
    Drag the right mouse button to change the window size
    Press W to reset window size
    Press S to reset scale
    Press R to reset both window size and scale
    Press P to toggle polar style graph

--- ComplexZeta.py
+++ ComplexZeta.py
@@ -2,56 +2,76 @@ import math
 from p5 import *
 from CoolColor import CC
 col=CC(255,0,0)
-scale=75
-polar=False #Toggles polar style graph
+orig_scale=100
+scale=orig_scale
+polar=False
 paused=False
 update=None
+window=Vector(600,600)
 def zeta(s,n):
     total=[1]
     for i in range(2,n+1):
         total.append(total[-1]+i**-s)
     return total
+def mouse_wheel(event):
+    global scale
+    scale+=int(event.scroll.y)
+    scale=abs(scale)
+def mouse_dragged(event):
+    if event.button=="RIGHT":
+        size(mouse_x,mouse_y)
 def mouse_pressed(event):
     global paused
     if event.button=="MIDDLE":
         paused=not paused
 def key_pressed(event):
-    global paused,update
+    global paused,update,scale,polar
     if event.key=="SPACE":
         paused=True
         update=complex(input("Complex number: "))
+    elif event.key=="R":
+        size(600,600)
+        scale=orig_scale
+    elif event.key=="S":
+        scale=orig_scale
+    elif event.key=="W":
+        size(window.x,window.y)
+    elif event.key=="P":
+        polar=not polar
 def setup():
-    size(600,600)
-    global trans
-    trans=Vector(width/2,height/2)
+    size(window.x,window.y)
 def draw():
-    global col,update
-    if paused and update==None:
-        return
+    global col,comp,update
+    trans=Vector(width/2,height/2)
     background(0)
     stroke(96)
     line((trans.x,height),(trans.x,0))
     line((0,trans.y),(width,trans.y))
-    #with push_matrix():
     translate(trans.x,trans.y)
     if polar:
-        rad=ceil((trans.x+trans.y)/2+scale)
-        for r in range(0,rad*2+scale,scale):
+        rad=ceil((trans.x+trans.y)/2)+scale
+        for r in range(scale,rad*2+scale,scale):
             no_fill()
-            ellipse((0,0),r,r)
+            ellipse((0,0),r*2,r*2)
+        rad*=2
         for t in [PI/4,PI/3,PI/6,0]:
             line((rad*cos(t),rad*sin(t)),(rad*cos(t+PI),rad*sin(t+PI)))
             line((rad*cos(-t),rad*sin(-t)),(rad*cos(-t+PI),rad*sin(-t+PI)))
     else:
         range_x=ceil(trans.x)
         range_y=ceil(trans.y)
-        for x in range(-range_x,range_x,scale):
+        for x in range(-scale,-range_x,-scale):
             line((x,5),(x,-5))
-        for y in range(-range_y,range_y,scale):
+        for x in range(scale,range_x,scale):
+            line((x,5),(x,-5))
+        for y in range(-scale,-range_y,-scale):
             line((5,y),(-5,y))
-    comp=complex(mouse_x-trans.x,-mouse_y+trans.y)
-    if update!=None:
-        comp=update*scale
+        for y in range(scale,range_y,scale):
+            line((5,y),(-5,y))
+    if not paused and update==None:
+        comp=complex(mouse_x-trans.x,-mouse_y+trans.y)
+        if update!=None:
+            comp=update*scale
     fill(255,255,0)
     ellipse((comp.real,-comp.imag),5,5)
     comps=zeta(comp/scale,201)
